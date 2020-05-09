a189c616121d0030366b01c5f40f419201475ca5
pbelisario_win <pedrobelisario1997@gmail.com>
2020-05-09 16:09:45 -0300

    Updates

---
+++ precache-manifest.9436d7fb3245816545f5d87bb8f84464.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b9ff5adacd0c06bbe6f3098c60f89ef",
+    "url": "/sorting-algo-web/index.html"
+  },
+  {
+    "revision": "1b2e14ba279014c9fc85",
+    "url": "/sorting-algo-web/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "003547864e5147a53cc2",
+    "url": "/sorting-algo-web/static/js/2.8788ad41.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/sorting-algo-web/static/js/2.8788ad41.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b2e14ba279014c9fc85",
+    "url": "/sorting-algo-web/static/js/main.659d05f3.chunk.js"
+  },
+  {
+    "revision": "5c78eda3d89d734be68d",
+    "url": "/sorting-algo-web/static/js/runtime-main.b8b2f903.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/sorting-algo-web/static/media/logo.5d5d9eef.svg"
+  }
+]);
